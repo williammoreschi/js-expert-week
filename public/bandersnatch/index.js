e09d56a8304e503cf43814cf88c15f8689be6b77
William Moreschi <williammoreschi@gmail.com>
2020-10-29 02:29:47 +0000

    Aula 2

--- public/bandersnatch/index.js
+++ public/bandersnatch/index.js
@@ -1,13 +1,20 @@
+const MANIFEST_URL = 'manifest.json'
+const localhost = ['127.0.0.1','localhost'];
+
 async function main() {
-    const player = videojs('vid');
-    const ModalDialog = videojs.getComponent('ModalDialog');
-    const modal = new ModalDialog(player, {
-        temporary: false, 
-        closeable: true
+    const isLocal = !!~localhost.indexOf(window.location.hostname);
+    const manifestJSON = await (await fetch(MANIFEST_URL)).json();
+    const host = isLocal ? manifestJSON.localhost : manifestJSON.productionHost;
+    
+    const videoComponent = new VideoComponent();
+    const network = new Network({host});
+    const videoPlayer = new VideoMediaPlayer({
+        manifestJSON,
+        network
     });
-
-    player.addChild(modal);
-
+    
+    videoPlayer.initializeCodec();
+    videoComponent.initializePlayer();
 }
 
 window.onload = main
